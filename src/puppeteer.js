b0243f20760db86f56f5d0982f6b676c6a2626b9
turovtsov <dturovtsov@marathonbet.ru>
2019-03-18 16:08:07 +0300

    add definition

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,11 +1,11 @@
 // @flow
 
 declare module 'puppeteer' {
-    declare type PuppeteerHTMLElementType = {};
+    declare type ElementHandleType = {};
 
     declare type PageScreenshotOptionsType = {
         path: string,
-    };
+    }
 
     declare class InterceptedRequest {
         resourceType(): string,
@@ -15,23 +15,28 @@ declare module 'puppeteer' {
 
     declare type WaitForNavigationOptionType = {
         timeout?: number,
-    };
+    }
+
+    declare type WaitForSelectorOptionType = {
+        timeout?: number,
+        visible?: boolean,
+        hidden?: boolean,
+    }
 
     declare class Page {
         goto(url: string): Promise<mixed>,
         screenshot(options: PageScreenshotOptionsType): Promise<mixed>,
         click(cssSelector: string): Promise<mixed>,
         type(cssSelector: string, text: string): Promise<mixed>,
-        $(cssSelector: string): Promise<PuppeteerHTMLElementType | null>,
-        evaluate<T>(funcOrStringFunc: (() => T) | string): Promise<T>,
+        $(cssSelector: string): Promise<?ElementHandleType>,
+        evaluate<T>(funcOrStringFunc: ((() => T) | string)): Promise<T>,
         url(): string,
         waitFor(timeoutInMs: number): Promise<mixed>,
-        setViewport(size: { width: number, height: number }): Promise<mixed>,
+        setViewport(size: {width: number, height: number}): Promise<mixed>,
         setRequestInterception(isEnable: boolean): Promise<mixed>,
-        on<T>(eventName: string, callback: (context: T) => mixed): mixed,
-        waitForNavigation(
-            options?: WaitForNavigationOptionType
-        ): Promise<mixed>,
+        on<T>(eventName: string, callback: (context: T)=> mixed): mixed,
+        waitForNavigation(options?: WaitForNavigationOptionType): Promise<mixed>,
+        waitForSelector(selector: string, options?: WaitForSelectorOptionType): Promise<?ElementHandleType>,
     }
 
     declare class Browser {
@@ -44,14 +49,14 @@ declare module 'puppeteer' {
         slowMo?: number,
 
         /*
-         * `--window-size=${width},${height}`,
-         * `--window-position=${leftPosition},${topPosition}`,
-         * '--disable-infobars',
-         * '--allow-insecure-localhost',
-         * '--disable-gpu',
-         * */
+        * `--window-size=${width},${height}`,
+        * `--window-position=${leftPosition},${topPosition}`,
+        * '--disable-infobars',
+        * '--allow-insecure-localhost',
+        * '--disable-gpu',
+        * */
         args?: Array<string>, // example ['--window-size=384,480', '--disable-infobars']
-    };
+    }
 
     declare function launch(launchOptions: LaunchOptionsType): Browser;
 }
