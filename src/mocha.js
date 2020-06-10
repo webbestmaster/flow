c3d0487fe09a41917122c58bcace830823578746
dim <web.best.master@gmail.com>
2020-06-10 14:32:59 +0300

    add new

--- src/mocha.js
+++ src/mocha.js
@@ -28,30 +28,59 @@ declare module 'mocha' {
     }
 
     declare interface $npm$mocha$ContextDefinition {
-        (description: string, callback: (/* this: $npm$mocha$SuiteCallbackContext */) => mixed): $npm$mocha$Suite,
-        only(description: string, callback: (/* this: $npm$mocha$SuiteCallbackContext */) => mixed): $npm$mocha$Suite,
-        skip(description: string, callback: (/* this: $npm$mocha$SuiteCallbackContext */) => mixed): void,
+        (description: string, callback: () => /* this: $npm$mocha$SuiteCallbackContext */ mixed): $npm$mocha$Suite,
+        only(description: string, callback: () => /* this: $npm$mocha$SuiteCallbackContext */ mixed): $npm$mocha$Suite,
+        skip(description: string, callback: () => /* this: $npm$mocha$SuiteCallbackContext */ mixed): void,
         timeout(ms: number): void,
     }
 
     declare type $npm$mocha$done = (error?: Error) => mixed;
 
     declare interface $npm$mocha$TestDefinition {
-        (expectation: string, callback?: (/* this: $npm$mocha$TestCallbackContext, */ done: $npm$mocha$done) => mixed): $npm$mocha$Test,
-        only(expectation: string, callback?: (/* this: $npm$mocha$TestCallbackContext, */ done: $npm$mocha$done) => mixed): $npm$mocha$Test,
-        skip(expectation: string, callback?: (/* this: $npm$mocha$TestCallbackContext, */ done: $npm$mocha$done) => mixed): void,
+        (
+            expectation: string,
+            callback?: (/* this: $npm$mocha$TestCallbackContext, */ done: $npm$mocha$done) => mixed,
+        ): $npm$mocha$Test,
+        only(
+            expectation: string,
+            callback?: (/* this: $npm$mocha$TestCallbackContext, */ done: $npm$mocha$done) => mixed,
+        ): $npm$mocha$Test,
+        skip(
+            expectation: string,
+            callback?: (/* this: $npm$mocha$TestCallbackContext, */ done: $npm$mocha$done) => mixed,
+        ): void,
         timeout(ms: number): void,
         state: TestStateType,
     }
 
-    declare export function before(callback: (/* this: $npm$mocha$HookCallbackContext, */ done: $npm$mocha$done) => mixed): void;
-    declare export function before(description: string, callback: (/* this: $npm$mocha$HookCallbackContext, */ done: $npm$mocha$done) => mixed): void;
-    declare export function after(callback: (/* this: $npm$mocha$HookCallbackContext, */ done: $npm$mocha$done) => mixed): void;
-    declare export function after(description: string, callback: (/* this: $npm$mocha$HookCallbackContext, */ done: $npm$mocha$done) => mixed): void;
-    declare export function beforeEach(callback: (/* this: $npm$mocha$BeforeAndAfterContext, */ done: $npm$mocha$done) => mixed): void;
-    declare export function beforeEach(description: string, callback: (/* this: $npm$mocha$BeforeAndAfterContext, */ done: $npm$mocha$done) => mixed): void;
-    declare export function afterEach(callback: (/* this: $npm$mocha$BeforeAndAfterContext, */ done: $npm$mocha$done) => mixed): void;
-    declare export function afterEach(description: string, callback: (/* this: $npm$mocha$BeforeAndAfterContext, */ done: $npm$mocha$done) => mixed): void;
+    declare export function before(
+        callback: (/* this: $npm$mocha$HookCallbackContext, */ done: $npm$mocha$done) => mixed,
+    ): void;
+    declare export function before(
+        description: string,
+        callback: (/* this: $npm$mocha$HookCallbackContext, */ done: $npm$mocha$done) => mixed,
+    ): void;
+    declare export function after(
+        callback: (/* this: $npm$mocha$HookCallbackContext, */ done: $npm$mocha$done) => mixed,
+    ): void;
+    declare export function after(
+        description: string,
+        callback: (/* this: $npm$mocha$HookCallbackContext, */ done: $npm$mocha$done) => mixed,
+    ): void;
+    declare export function beforeEach(
+        callback: (/* this: $npm$mocha$BeforeAndAfterContext, */ done: $npm$mocha$done) => mixed,
+    ): void;
+    declare export function beforeEach(
+        description: string,
+        callback: (/* this: $npm$mocha$BeforeAndAfterContext, */ done: $npm$mocha$done) => mixed,
+    ): void;
+    declare export function afterEach(
+        callback: (/* this: $npm$mocha$BeforeAndAfterContext, */ done: $npm$mocha$done) => mixed,
+    ): void;
+    declare export function afterEach(
+        description: string,
+        callback: (/* this: $npm$mocha$BeforeAndAfterContext, */ done: $npm$mocha$done) => mixed,
+    ): void;
     declare export var describe: $npm$mocha$ContextDefinition;
     declare export var xdescribe: $npm$mocha$ContextDefinition;
     declare export var context: $npm$mocha$ContextDefinition;
@@ -61,4 +90,3 @@ declare module 'mocha' {
     declare var test: $npm$mocha$TestDefinition;
     declare var specify: $npm$mocha$TestDefinition;
 }
-
--- src/mongodb.js
+++ src/mongodb.js
@@ -3,7 +3,7 @@
 declare module 'mongodb' {
     declare export type MongoSortDirectionType = 1 | -1;
 
-    declare type MongoClientConnectCallBackType = (connectError: Error | null, client: MongoClient | null) => mixed;
+    declare type MongoClientConnectCallBackType = (connectError: ?Error, client: ?MongoClient) => mixed;
 
     declare type MongoCollectionActionOperationType = {};
 
@@ -15,17 +15,16 @@ declare module 'mongodb' {
         transform?: (item: ItemType) => mixed,
     };
 
-    declare type MongoCollectionCursorIteratorType<ItemType> = (
-        mayBeError: Error | null,
-        item: ItemType | null,
-    ) => mixed;
+    declare type MongoCollectionCursorIteratorType<ItemType> = (error: ?Error, item: ?ItemType) => mixed;
 
     declare export class MongoCollectionCursor<ItemType> {
         each: MongoCollectionCursorIteratorType<ItemType>,
-        stream: (option: MongoCollectionCursorStreamOptionType<ItemType>) => WritableStream,
+        stream: (option: MongoCollectionCursorStreamOptionType<ItemType>) => MongoCollectionCursor<ItemType>,
         limit: (size: number) => MongoCollectionCursor<ItemType>,
         skip: (size: number) => MongoCollectionCursor<ItemType>,
         sort: (sort: {[key: string]: MongoSortDirectionType}) => MongoCollectionCursor<ItemType>,
+        toArray: (callBack: (error: ?Error, array: ?Array<ItemType>) => mixed) => mixed,
+        pipe: (destination: mixed) => mixed,
     }
 
     declare type MongoCollectionFindOption = {
@@ -35,14 +34,35 @@ declare module 'mongodb' {
         +maxTimeMS?: number,
     };
 
+    declare type SearchParametersType = {|
+        +$text: {|
+            +$search?: string,
+            +$language?: string,
+            +$caseSensitive?: boolean,
+            +$diacriticSensitive?: boolean,
+        |},
+    |};
+
     declare export class MongoCollection<ItemType> {
         insertOne: (item: ItemType) => Promise<MongoCollectionActionResultType>,
         insertMany: (itemList: Array<ItemType>) => Promise<MongoCollectionActionResultType>,
-        find: (item: $Shape<ItemType>, options?: MongoCollectionFindOption) => MongoCollectionCursor<ItemType>,
-        findOne: (item: $Shape<ItemType>) => Promise<ItemType | null>,
-        updateMany: (filter: $Shape<ItemType>, update: {}, options: {}, callBack: () => mixed) => mixed,
-        updateOne: (filter: $Shape<ItemType>, update: {}, options: {}, callBack: () => mixed) => mixed,
+        find: (
+            filter: $Shape<ItemType> | SearchParametersType,
+            options?: MongoCollectionFindOption,
+        ) => MongoCollectionCursor<ItemType>,
+        findOne: (filter: $Shape<ItemType>) => Promise<ItemType | null>,
+        updateMany: (
+            filter: $Shape<ItemType>,
+            update: {},
+            options: {},
+        ) => Promise<Error | MongoCollectionActionResultType>,
+        updateOne: (
+            filter: $Shape<ItemType>,
+            update: {},
+            options: {},
+        ) => Promise<Error | MongoCollectionActionResultType>,
         countDocuments: () => Promise<number>,
+        deleteOne: (filter: $Shape<ItemType>, options: {}) => Promise<mixed>,
     }
 
     declare export class MongoDataBase {
@@ -55,12 +75,8 @@ declare module 'mongodb' {
     };
 
     declare export class MongoClient {
-        static connect: (
-            url: string,
-            option?: MongoClientOptionType,
-            callBack: MongoClientConnectCallBackType,
-        ) => mixed,
+        static connect: (url: string, option: MongoClientOptionType, callBack: MongoClientConnectCallBackType) => mixed,
         db: (dataBaseName: string) => MongoDataBase,
-        close: (closeCallBack: (Error | null) => mixed) => mixed,
+        close: (closeCallBack: (error: ?Error) => mixed) => mixed,
     }
 }
